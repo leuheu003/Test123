2dc8eaf73ab54864c4ed9c596bdd22e5feab37e6
Pham Duc Long <longpdgch17143@fpt.edu.vn>
2019-08-28 16:31:08 +0700

    da them

---
+++ menu_admin.php
@@ -0,0 +1,21 @@
+<nav>
+    <ul>
+        <li class="selected"><a href="index.php">Home</a></li>
+        <li>
+            <a href="">Manage Catalogue</a>
+            <ul>
+                <li><a href="addcatalogue.php">Add Catalogue</a></li>
+                <li><a href="loadcatalogue.php">View/Search/Update/Delete catalogue</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="">Manage Items</a>
+            <ul>
+                <li><a href="additem.php">Add Item</a></li>
+                <li><a href="loaditem.php">View/Search/Update/Delete item</a></li>
+            </ul>
+        </li>
+        <li><a href="logout.php">Logout <?php echo $userstr; ?></a></li>
+    </ul>
+</nav>
+
