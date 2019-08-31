1530a3121fb48cd7572c0ea17bac22608caec2f9
Duc Long <52957488+leuheu003@users.noreply.github.com>
2019-08-31 09:35:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -112,11 +112,11 @@ $result = queryMysql($query);
                 
                 <div class="nav">
                     <ul>
-                        <center<li><a href="./index.php">Home</a></li></center>
-                        <center<li><a href="./header.php">Admin</a></li></center>
-                        <center<li><a href="#Lego">Lego</a></li></center>
-                        <center<li><a href="#Figure">Figure</a></li></center>
-                        <center<li><a href="#Clay">Clay</a></li></center>
+                        <right<li><a href="./index.php">Home</a></li></right>
+                        <right<li><a href="./header.php">Admin</a></li></right>
+                        <right<li><a href="#Lego">Lego</a></li></right>
+                        <right<li><a href="#Figure">Figure</a></li></right>
+                        <right<li><a href="#Clay">Clay</a></li></right>
 
                     </ul>
 
