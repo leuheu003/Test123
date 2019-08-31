7d3c03a0bc58685890b14ee4252a9e922c3d1cc4
Duc Long <52957488+leuheu003@users.noreply.github.com>
2019-08-31 15:28:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
                 display: block;
             }
             .nav li{
-                float: center;
+                float: left;
             }
             .nav a:hover{
                 color: black;
@@ -112,11 +112,13 @@ $result = queryMysql($query);
                 
                 <div class="nav">
                     <ul>
+                    <left>
                         <li><a href="./index.php">Home</a></li>
                         <li><a href="#Lego">Lego</a></li>
                         <li><a href="#Figure">Figure</a></li>
                         <li><a href="#Clay">Clay</a></li>
-                        <li><a href="./header.php">Manage</a></li>
+                        <li><a href="./header.php">Manage</a></li>\
+                    </left>
                     </ul>
 
 
