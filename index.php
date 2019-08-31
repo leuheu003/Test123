fe6e1b44b80efe919014633e82a092f69df913ea
Duc Long <52957488+leuheu003@users.noreply.github.com>
2019-08-31 15:17:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -86,6 +86,9 @@
                 padding: 0 15px;
                 display: block;
             }
+            .nav li{
+                float: inside;
+            }
             .nav a:hover{
                 color: black;
             }
